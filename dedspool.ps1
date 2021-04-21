39e6d4d29898055628cbf32c3326f8b4601c8364
cturpin <cturpin@kihomac.com>
2021-04-21 09:46:29 -0600

    New-PSSession now allows user input That line also handles Get-Credential - Moved try/catch to the file deletion specifically

--- dedspool.ps1
+++ dedspool.ps1
@@ -1,21 +1,28 @@
-﻿# Clear all sessions
+﻿# dedspool.ps1 remote-execution powershell script
+# By Grinny, April 2021
+# Got a print server that's being stupid? Dedspool it.
+# All you need is an admin username and password for that machine
+# Organically coded with all-natural ingredients
+# Only tested on prod servers because I live on the edge
+# ========================================
+
+# Clear all sessions
 #Invoke-Expression -Command "Get-PSSession | Remove-PSSession"
-$cred = Get-Credential # Set up a new credential
-$ses = New-PSSession -Computername PRINTSRV -Credential $cred -Name "dedspool"# Set up session with credentials
+#$cred = Get-Credential # Set up a new credential
+$ses = New-PSSession -Computername (Read-Host "Enter print server computer name") -Credential (Get-Credential) -Name "dedspool"# Set up session with credentials
 #Enter-PSSession $ses # creates interactive session
 Invoke-Command -Session $ses -ScriptBlock {
+    Write-Host "Stopping spooler"
+    Stop-Service -Force -Name "Spooler"
+    Set-Location "C:\Windows\System32\spool\PRINTERS"
+    Write-Host "Clearing C:\Windows\System32\spool\PRINTERS directory"
     try {
-        Write-Host "Stopping spooler"
-        Stop-Service -Force -Name "Spooler"
-        Set-Location "C:\Windows\System32\spool\PRINTERS"
-        Write-Host "Clearing C:\Windows\System32\spool\PRINTERS directory"
         Remove-Item .\*
-        Start-Service -Name "Spooler"
-        Write-Host "Spooler restarted"
-    } catch {
-        Write-Host $error
-        Write-Host "it dun broke"
+    } catch  {
+        Write-Host "The print server's spool file(s) are actively in use.`nTry again later."
     }
+    Start-Service -Name "Spooler"
+    Write-Host "Spooler restarted"
 }
 #Get-PSSession # debug
 Invoke-Expression -Command "Get-PSSession | Remove-PSSession"
